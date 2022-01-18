50c906ca4417455383d3d7d1df39a494787a62e5
nandinig2001 <76276520+nandinig2001@users.noreply.github.com>
2022-01-18 23:30:12 +0530

    Update show.blade.php

---
+++ resources/views/recipes/show.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('title', 'Recipe | Foodie')
+
+@section('styles')
+<style>
+
+
+    </style>
+@endsection
+
+@section('content')
+
+
+<!-- Page Content -->
+<div class="container">
+    <div class="section">
+
+    <!-- Portfolio Item Row -->
+    <div class="row recipe-searched-results">
+
+    </div>
+    <!-- /.row -->
+
+    <!-- Related Projects Row -->
+    <h3 class="my-5">Suggested Recipes</h3>
+
+    <div class="row other-recipes">
+
+
+    </div>
+    <!-- /.row -->
+
+    </div>
+
+  </div>
+  <!-- /.container -->
+
+
+
+
+@endsection
+
